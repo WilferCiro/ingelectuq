4e931a49280a524392c8f4ca0d2768e2ae5ea525
Wilfer Ciro <wilferciro.option@latam.com>
2024-08-24 11:07:27 -0500

    feat: add layout data

--- src/data/layout-data.ts
+++ src/data/layout-data.ts
@@ -1,6 +1,6 @@
 export const layoutData = {
   description:
-    "Soy un apasionado desarrollador fullstack con experiencia en tecnologías web modernas. Me encanta crear aplicaciones web innovadoras y soluciones tecnológicas para problemas complejos",
+    "DESCRIPCIÓIN",
   keywords:
-    "Wilfer Daniel Ciro Maya, desarrollador, desarrollador fullstack, portafolio, sobre mi, nodejs, react, python",
+    "universidad del quindío, colombia, ingeniería electrónica, acreditación institucional, acreditación internacional, arcu-sur, mercosur, cna",
 } as const;
