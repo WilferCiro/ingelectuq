f3bf2f1cfa16d549246ea1649d435a552b6e76db
Wilfer Ciro <wilferciro.option@latam.com>
2024-08-27 22:02:29 -0500

    feat: add video and some improvements

--- src/data/layout-data.ts
+++ src/data/layout-data.ts
@@ -1,6 +1,6 @@
 export const layoutData = {
   description:
-    "DESCRIPCIÓIN",
+    "Conoce más sobre nuestro proceso para obtener la acreditación internacional por ARCUSUR y la reacreditación por la CNA. Descubre cómo nos comprometemos con la excelencia académica y la calidad educativa. Visita nuestra web para más detalles.",
   keywords:
     "universidad del quindío, colombia, ingeniería electrónica, acreditación institucional, acreditación internacional, arcu-sur, mercosur, cna",
 } as const;
